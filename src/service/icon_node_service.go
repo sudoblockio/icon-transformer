cad10d62a8d0b8b5563ff154e0069aeb9d9f11e0
robcxyz <rob@geometrylabs.io>
2022-04-17 20:19:14 -0700

    chore: cleanup old icon_node_service

--- src/service/icon_node_service.go
+++ src/service/icon_node_service.go
@@ -68,99 +68,6 @@ func IconNodeServiceGetBlockTransactionHashes(height int) (*[]string, error) {
 	return &transactionHashes, nil
 }
 
-//func IconNodeServiceGetBlockTransactionHashes(height int) (*[]string, error) {
-//
-//	// Request icon contract
-//	url := config.Config.IconNodeServiceURL
-//	method := "POST"
-//	payload := fmt.Sprintf(`{
-//    "jsonrpc": "2.0",
-//    "method": "icx_getBlockByHeight",
-//    "id": 1,
-//    "params": {
-//        "height": "0x%x"
-//    }
-//	}`, height)
-//
-//	// Create http client
-//	client := &http.Client{}
-//	req, err := http.NewRequest(method, url, strings.NewReader(payload))
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	// Execute request
-//	req.Header.Add("Content-Type", "application/json")
-//	res, err := client.Do(req)
-//	if err != nil {
-//		return nil, err
-//	}
-//	defer res.Body.Close()
-//
-//	// Read body
-//	bodyString, err := ioutil.ReadAll(res.Body)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	// Check status code
-//	if res.StatusCode != 200 {
-//		return nil, errors.New(
-//			"StatusCode=" + strconv.Itoa(res.StatusCode) +
-//				",Request=" + payload +
-//				",Response=" + string(bodyString),
-//		)
-//	}
-//
-//	// Parse body
-//	body := map[string]interface{}{}
-//	err = json.Unmarshal(bodyString, &body)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	// Extract result
-//	result, ok := body["result"].(map[string]interface{})
-//	if ok == false {
-//		return nil, errors.New("Cannot read result")
-//	}
-//
-//	// Extract transactions
-//	transactions, ok := result["confirmed_transaction_list"].([]interface{})
-//	if ok == false {
-//		return nil, errors.New("Cannot read confirmed_transaction_list")
-//	}
-//
-//	// Extract transaciton hashes
-//	transactionHashes := []string{}
-//	for _, t := range transactions {
-//		tx, ok := t.(map[string]interface{})
-//		if ok == false {
-//			return nil, errors.New("1 Cannot read transaction hash from block #" + fmt.Sprintf("%x", height))
-//		}
-//
-//		// V1
-//		hash, ok := tx["tx_hash"].(string)
-//		if ok == true {
-//			transactionHashes = append(transactionHashes, "0x"+hash)
-//			continue
-//		}
-//
-//		// V3
-//		hash, ok = tx["txHash"].(string)
-//		if ok == true {
-//			transactionHashes = append(transactionHashes, hash)
-//			continue
-//		}
-//
-//		if ok == false {
-//			return nil, errors.New("2 Cannot read transaction hash from block #" + fmt.Sprintf("%x", height))
-//		}
-//	}
-//
-//	return &transactionHashes, nil
-//}
-
 func IconNodeServiceGetTokenDecimalBase(tokenContractAddress string) (int, error) {
 
 	// Redis cache
@@ -210,7 +117,6 @@ func IconNodeServiceGetTokenDecimalBase(tokenContractAddress string) (int, error
 }
 
 func IconNodeServiceGetTokenContractName(tokenContractAddress string) (string, error) {
-
 	// Redis cache
 	redisCacheKey := config.Config.RedisKeyPrefix + "token_contract_name_" + tokenContractAddress
 	tokenContractName, err := redis.GetRedisClient().GetValue(redisCacheKey)
@@ -235,42 +141,6 @@ func IconNodeServiceGetTokenContractName(tokenContractAddress string) (string, e
     }
 	}`, tokenContractAddress)
 
-	//// Create http client
-	//client := &http.Client{}
-	//req, err := http.NewRequest(method, url, strings.NewReader(payload))
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//// Execute request
-	//req.Header.Add("Content-Type", "application/json")
-	//res, err := client.Do(req)
-	//if err != nil {
-	//	return "", err
-	//}
-	//defer res.Body.Close()
-	//
-	//// Read body
-	//bodyString, err := ioutil.ReadAll(res.Body)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//// Check status code
-	//if res.StatusCode != 200 {
-	//	return "", errors.New(
-	//		"StatusCode=" + strconv.Itoa(res.StatusCode) +
-	//			",Request=" + payload +
-	//			",Response=" + string(bodyString),
-	//	)
-	//}
-	//
-	//// Parse body
-	//body := map[string]interface{}{}
-	//err = json.Unmarshal(bodyString, &body)
-	//if err != nil {
-	//	return "", err
-	//}
 	body, err := JsonRpcRequestWithRetry(payload)
 	if err != nil {
 		zap.S().Fatal(err)
@@ -293,7 +163,6 @@ func IconNodeServiceGetTokenContractName(tokenContractAddress string) (string, e
 }
 
 func IconNodeServiceGetTokenContractSymbol(tokenContractAddress string) (string, error) {
-
 	// Redis cache
 	redisCacheKey := config.Config.RedisKeyPrefix + "token_contract_symbol_" + tokenContractAddress
 	tokenContractSymbol, err := redis.GetRedisClient().GetValue(redisCacheKey)
@@ -304,8 +173,6 @@ func IconNodeServiceGetTokenContractSymbol(tokenContractAddress string) (string,
 	}
 
 	// Request icon contract
-	//url := config.Config.IconNodeServiceURL
-	//method := "POST"
 	payload := fmt.Sprintf(`{
     "jsonrpc": "2.0",
     "id": 1234,
@@ -319,43 +186,6 @@ func IconNodeServiceGetTokenContractSymbol(tokenContractAddress string) (string,
         }
     }
 	}`, tokenContractAddress)
-
-	// Create http client
-	//client := &http.Client{}
-	//req, err := http.NewRequest(method, url, strings.NewReader(payload))
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//// Execute request
-	//req.Header.Add("Content-Type", "application/json")
-	//res, err := client.Do(req)
-	//if err != nil {
-	//	return "", err
-	//}
-	//defer res.Body.Close()
-	//
-	//// Read body
-	//bodyString, err := ioutil.ReadAll(res.Body)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//// Check status code
-	//if res.StatusCode != 200 {
-	//	return "", errors.New(
-	//		"StatusCode=" + strconv.Itoa(res.StatusCode) +
-	//			",Request=" + payload +
-	//			",Response=" + string(bodyString),
-	//	)
-	//}
-	//
-	//// Parse body
-	//body := map[string]interface{}{}
-	//err = json.Unmarshal(bodyString, &body)
-	//if err != nil {
-	//	return "", err
-	//}
 
 	body, err := JsonRpcRequestWithRetry(payload)
 	if err != nil {
@@ -379,9 +209,6 @@ func IconNodeServiceGetTokenContractSymbol(tokenContractAddress string) (string,
 }
 
 func IconNodeServiceGetBalance(publicKey string) (string, error) {
-
-	//url := config.Config.IconNodeServiceURL
-	//method := "POST"
 	payload := fmt.Sprintf(`{
     "jsonrpc": "2.0",
     "method": "icx_getBalance",
@@ -391,42 +218,6 @@ func IconNodeServiceGetBalance(publicKey string) (string, error) {
     }
 	}`, publicKey)
 
-	//// Create http client
-	//client := &http.Client{}
-	//req, err := http.NewRequest(method, url, strings.NewReader(payload))
-	//if err != nil {
-	//	return "0x0", err
-	//}
-	//
-	//// Execute request
-	//req.Header.Add("Content-Type", "application/json")
-	//res, err := client.Do(req)
-	//if err != nil {
-	//	return "0x0", err
-	//}
-	//defer res.Body.Close()
-	//
-	//// Read body
-	//bodyString, err := ioutil.ReadAll(res.Body)
-	//if err != nil {
-	//	return "0x0", err
-	//}
-	//
-	//// Check status code
-	//if res.StatusCode != 200 {
-	//	return "0x0", errors.New(
-	//		"StatusCode=" + strconv.Itoa(res.StatusCode) +
-	//			",Request=" + payload +
-	//			",Response=" + string(bodyString),
-	//	)
-	//}
-	//
-	//// Parse body
-	//body := map[string]interface{}{}
-	//err = json.Unmarshal(bodyString, &body)
-	//if err != nil {
-	//	return "0x0", err
-	//}
 	body, err := JsonRpcRequestWithRetry(payload)
 	if err != nil {
 		zap.S().Fatal(err)
@@ -442,9 +233,6 @@ func IconNodeServiceGetBalance(publicKey string) (string, error) {
 }
 
 func IconNodeServiceGetStakedBalance(publicKey string) (string, error) {
-
-	//url := config.Config.IconNodeServiceURL
-	//method := "POST"
 	payload := fmt.Sprintf(`{
     "jsonrpc": "2.0",
     "id": 1234,
@@ -460,43 +248,6 @@ func IconNodeServiceGetStakedBalance(publicKey string) (string, error) {
         }
     }
 	}`, publicKey)
-
-	// Create http client
-	//client := &http.Client{}
-	//req, err := http.NewRequest(method, url, strings.NewReader(payload))
-	//if err != nil {
-	//	return "0x0", err
-	//}
-	//
-	//// Execute request
-	//req.Header.Add("Content-Type", "application/json")
-	//res, err := client.Do(req)
-	//if err != nil {
-	//	return "0x0", err
-	//}
-	//defer res.Body.Close()
-	//
-	//// Read body
-	//bodyString, err := ioutil.ReadAll(res.Body)
-	//if err != nil {
-	//	return "0x0", err
-	//}
-	//
-	//// Check status code
-	//if res.StatusCode != 200 {
-	//	return "0x0", errors.New(
-	//		"StatusCode=" + strconv.Itoa(res.StatusCode) +
-	//			",Request=" + payload +
-	//			",Response=" + string(bodyString),
-	//	)
-	//}
-	//
-	//// Parse body
-	//body := map[string]interface{}{}
-	//err = json.Unmarshal(bodyString, &body)
-	//if err != nil {
-	//	return "0x0", err
-	//}
 
 	body, err := JsonRpcRequestWithRetry(payload)
 	if err != nil {
@@ -518,10 +269,7 @@ func IconNodeServiceGetStakedBalance(publicKey string) (string, error) {
 }
 
 func IconNodeServiceGetTokenBalance(tokenContractAddress string, tokenHolderAddress string) (string, error) {
-
 	// Request icon contract
-	//url := config.Config.IconNodeServiceURL
-	//method := "POST"
 	payload := fmt.Sprintf(`{
     "jsonrpc": "2.0",
     "id": 1234,
@@ -535,43 +283,6 @@ func IconNodeServiceGetTokenBalance(tokenContractAddress string, tokenHolderAddr
         }
     }
 	}`, tokenContractAddress, tokenHolderAddress)
-
-	//// Create http client
-	//client := &http.Client{}
-	//req, err := http.NewRequest(method, url, strings.NewReader(payload))
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//// Execute request
-	//req.Header.Add("Content-Type", "application/json")
-	//res, err := client.Do(req)
-	//if err != nil {
-	//	return "", err
-	//}
-	//defer res.Body.Close()
-	//
-	//// Read body
-	//bodyString, err := ioutil.ReadAll(res.Body)
-	//if err != nil {
-	//	return "", err
-	//}
-	//
-	//// Check status code
-	//if res.StatusCode != 200 {
-	//	return "", errors.New(
-	//		"StatusCode=" + strconv.Itoa(res.StatusCode) +
-	//			",Request=" + payload +
-	//			",Response=" + string(bodyString),
-	//	)
-	//}
-	//
-	//// Parse body
-	//body := map[string]interface{}{}
-	//err = json.Unmarshal(bodyString, &body)
-	//if err != nil {
-	//	return "", err
-	//}
 
 	body, err := JsonRpcRequestWithRetry(payload)
 	if err != nil {
