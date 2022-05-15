5fc15912fd8352d94f40136dd01617d79b2b0026
robcxyz <rob@geometrylabs.io>
2022-05-15 15:37:14 -0700

    chore: update redis recovery script - snub old

--- src/routines/redis_recovery.go
+++ src/routines/redis_recovery.go
@@ -1,31 +1,60 @@
 package routines
 
 import (
-	"github.com/sudoblockio/icon-transformer/crud"
-	"github.com/sudoblockio/icon-transformer/redis"
 	"go.uber.org/zap"
 )
 
 func StartRedisRecovery() {
 
 	// Missing Keys
-	go redisRecovery()
-
-}
-
-func redisRecovery() {
-
-	redisKeys, err := crud.GetRedisKeyCrud().SelectAll()
-	if err != nil {
-		zap.S().Fatal(err.Error())
-	}
-
-	for _, redisKey := range *redisKeys {
-		err = redis.GetRedisClient().SetValue(redisKey.Key, redisKey.Value)
+	//go redisRecovery()
+	zap.S().Info("Starting redis recovery...")
+	// Block count
+	go func() {
+		err := blockCountExec()
 		if err != nil {
-			zap.S().Fatal(err.Error())
+			zap.S().Panic("Block count: ", err.Error())
 		}
-	}
+	}()
 
-	zap.S().Info("Routine=redisRecovery - Recovered redis")
+	// Transaction regular count
+	go func() {
+		err := transactionRegularCountExec()
+		if err != nil {
+			zap.S().Panic("Transaction count: ", err.Error())
+		}
+	}()
+
+	// Token transfer count
+	go func() {
+		err := tokenTransferCountExec()
+		if err != nil {
+			zap.S().Panic("Token transfer count: ", err.Error())
+		}
+	}()
+
+	// By address transaction
+	go func() {
+		err := byAddressCountExec()
+		if err != nil {
+			zap.S().Panic("By address counts: ", err.Error())
+		}
+	}()
 }
+
+//func redisRecovery() {
+//
+//	redisKeys, err := crud.GetRedisKeyCrud().SelectAll()
+//	if err != nil {
+//		zap.S().Fatal(err.Error())
+//	}
+//
+//	for _, redisKey := range *redisKeys {
+//		err = redis.GetRedisClient().SetValue(redisKey.Key, redisKey.Value)
+//		if err != nil {
+//			zap.S().Fatal(err.Error())
+//		}
+//	}
+//
+//	zap.S().Info("Routine=redisRecovery - Recovered redis")
+//}
---
+++ src/routines/redis_recovery_test.go
@@ -0,0 +1,11 @@
+package routines
+
+import (
+	"github.com/sudoblockio/icon-transformer/config"
+	"testing"
+)
+
+func TestRedisRecovery(t *testing.T) {
+	config.ReadTestEnvironment()
+	StartRedisRecovery()
+}
