29883a3955400e12546ac4632b503f455400c2bf
robcxyz <rob@geometrylabs.io>
2022-05-23 14:32:58 -0700

    chore: make routines sequential due to ooming

--- src/routines/routines.go
+++ src/routines/routines.go
@@ -2,48 +2,73 @@ package routines
 
 func StartRoutines() {
 
-	//// Address Balance
+	////// Address Balance
 	//go addressBalanceRoutine()
+	//
+	//// Governance add preps
+	//go addressIsPrep()
+	//
+	//// Address Type
+	//go addressTypeRoutine()
+	//
+	//// Address Count
+	//go addressCountRoutine()
+	//
+	//// Address Transaction Count
+	//go addressTransactionCountRoutine()
+	//
+	//// Address Transaction Internal Count
+	//go addressTransactionInternalCountRoutine()
+	//
+	//// Address Log Count
+	//go addressLogCountRoutine()
+	//
+	//// Address Token Transfer Count
+	//go addressTokenTransferCountRoutine()
+	//
+	//// Address Token Balance
+	//go tokenAddressBalanceRoutine()
+	//
+	//// Address Token Count
+	//go tokenAddressCountRoutine()
+	//
+	//// Transaction Create Score
+	//go transactionCreateScoreRoutine()
+	//
+	//// Redis Store Count Keys
+	//go redisStoreKeysRoutine()
+	//
+	//// Block count
+	//go blockCountRoutine()
+	//
+	//// Transaction regular count
+	//go transactionRegularCountRoutine()
+	//
+	//// Token transfer count
+	//go tokenTransferCountRoutine()
 
-	// Governance add preps
-	go addressIsPrep()
-
-	// Address Type
-	go addressTypeRoutine()
-
-	// Address Count
-	go addressCountRoutine()
-
-	// Address Transaction Count
-	go addressTransactionCountRoutine()
-
-	// Address Transaction Internal Count
-	go addressTransactionInternalCountRoutine()
-
-	// Address Log Count
-	go addressLogCountRoutine()
-
-	// Address Token Transfer Count
-	go addressTokenTransferCountRoutine()
-
-	// Address Token Balance
-	go tokenAddressBalanceRoutine()
-
-	// Address Token Count
-	go tokenAddressCountRoutine()
-
+	addressBalanceRoutine()
+	addressIsPrep()
+	addressTypeRoutine()
+	addressCountRoutine()
+	addressTransactionCountRoutine()
+	addressTransactionInternalCountRoutine()
+	addressLogCountRoutine()
+	addressTokenTransferCountRoutine()
+	tokenAddressBalanceRoutine()
+	tokenAddressCountRoutine()
 	// Transaction Create Score
-	go transactionCreateScoreRoutine()
+	transactionCreateScoreRoutine()
 
 	// Redis Store Count Keys
-	go redisStoreKeysRoutine()
+	redisStoreKeysRoutine()
 
 	// Block count
-	go blockCountRoutine()
+	blockCountRoutine()
 
 	// Transaction regular count
-	go transactionRegularCountRoutine()
+	transactionRegularCountRoutine()
 
 	// Token transfer count
-	go tokenTransferCountRoutine()
+	tokenTransferCountRoutine()
 }
