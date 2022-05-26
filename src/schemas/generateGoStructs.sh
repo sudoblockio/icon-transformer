91347f56ecb683255ef8eb7d678f54b7a320036a
robcxyz <rob@geometrylabs.io>
2022-05-25 19:49:47 -0700

    chore: add new index and refactor schemas so that they can be genned with local gorm dependency

--- src/schemas/generateGoStructs.sh
+++ src/schemas/generateGoStructs.sh
@@ -1,12 +1,23 @@
 echo "Remove old models..."
-rm ../models/*
+set -e
+
+#rm ../models/*
+cd "$(dirname "$0")"
 
 echo "Starting proto to struct..."
 export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
+#protoc -I=. -I=$GOPATH/src --go_out=.. --gorm_out=engine=postgres:.. *.proto
 
-protoc -I=. -I=$GOPATH/src --go_out=.. --gorm_out=engine=postgres:.. *.proto
+protoc \
+  --proto_path=. \
+  --proto_path=${GOPATH}/src \
+  --proto_path=${GOPATH}/src/github.com/mwitkow/go-proto-validators \
+  --proto_path=${GOPATH}/src/github.com/infobloxopen/protoc-gen-gorm/options \
+  --go_out=.. \
+  --gorm_out="engine=postgres:.." \
+  *.proto
 
 # Remove omitempty option
 # Credit: https://stackoverflow.com/a/37335452
