84a131d4ffe7484beaf0deecacedd5936be16f8d
robcxyz <rob@geometrylabs.io>
2022-06-13 19:34:43 -0600

    chore: update go structs script

--- src/schemas/generateGoStructs.sh
+++ src/schemas/generateGoStructs.sh
@@ -1,9 +1,10 @@
-echo "Remove old models..."
 set -e
 
 # For compiling issues
 # https://www.notion.so/Protobuf-d5efc374891a452798e3f3a414722eec
 
+# This could be run to nuke it all
+#echo "Remove old models..."
 #rm ../models/*
 cd "$(dirname "$0")"
 
@@ -13,6 +14,8 @@ export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOROOT:$GOPATH:$GOBIN
 #protoc -I=. -I=$GOPATH/src --go_out=.. --gorm_out=engine=postgres:.. *.proto
 
+# Enable extended glob
+shopt -s extglob
 protoc \
   --proto_path=. \
   --proto_path=${GOPATH}/src \
@@ -20,7 +23,15 @@ protoc \
   --proto_path=${GOPATH}/src/github.com/infobloxopen/protoc-gen-gorm/options \
   --go_out=.. \
   --gorm_out="engine=postgres:.." \
-  *.proto
+  !(*contract_processed).proto
+
+# For contract_processed.proto which has an optional field
+protoc \
+  --proto_path=. \
+  --proto_path=${GOPATH}/src \
+  --proto_path=${GOPATH}/src/github.com/mwitkow/go-proto-validators \
+  --go_out=.. \
+  contract_processed.proto
 
 # Remove omitempty option
 # Credit: https://stackoverflow.com/a/37335452
