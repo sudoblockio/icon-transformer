8260238d0de7a6c2c5ab38637fea240b7cee9c56
robcxyz <rob@geometrylabs.io>
2022-07-20 17:36:22 -0600

    chore: add token tx count to routine

--- src/routines/transactions.go
+++ src/routines/transactions.go
@@ -7,7 +7,9 @@ import (
 	"go.uber.org/zap"
 )
 
+
 func setTransactionCounts() {
+	// Regular Txs
 	count, err := crud.GetTransactionCrud().CountRegular()
 	if err != nil {
 		zap.S().Fatal(err.Error())
@@ -20,12 +22,26 @@ func setTransactionCounts() {
 		zap.S().Fatal(err.Error())
 	}
 
+	// Internal Txs
 	count, err = crud.GetTransactionCrud().CountInternal()
 	if err != nil {
 		zap.S().Fatal(err.Error())
 	}
 	err = redis.GetRedisClient().SetCount(
 		config.Config.RedisKeyPrefix+"transaction_internal_count",
+		count,
+	)
+	if err != nil {
+		zap.S().Fatal(err.Error())
+	}
+
+	// Token transfer Txs
+	count, err = crud.GetTokenTransferCrud().Count()
+	if err != nil {
+		zap.S().Fatal(err.Error())
+	}
+	err = redis.GetRedisClient().SetCount(
+		config.Config.RedisKeyPrefix+"token_transfer_count",
 		count,
 	)
 	if err != nil {
