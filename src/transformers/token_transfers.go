701c2a3534fa4a5754060a3e74f4361f7ba8fbd3
robcxyz <rob@geometrylabs.io>
2022-08-01 11:27:56 -0600

    feat: add irc31 TransferSingle method for token transfers

--- src/transformers/token_transfers.go
+++ src/transformers/token_transfers.go
@@ -14,7 +14,7 @@ import (
 func findHighestIcxTransfer(logs []*models.LogETL) (string, float64) {
 
 	var highestIcxTransfer float64 = 0
-	var highestIcxTransferValue = ""
+	var highestIcxTransferValue = "0x0"
 	for _, v := range logs {
 		if v.Indexed[0] == "ICXTransfer(Address,Address,int)" {
 			icxValue := v.Indexed[3]
@@ -121,7 +121,7 @@ func transformBlockETLToTokenTransfers(blockETL *models.BlockETL) []*models.Toke
 				// To Address
 				toAddress := logETL.Indexed[2]
 
-				//// Value
+				// Value
 				value, valueDecimal := findHighestIcxTransfer(transactionETL.Logs)
 
 				// NFT ID
@@ -167,6 +167,67 @@ func transformBlockETLToTokenTransfers(blockETL *models.BlockETL) []*models.Toke
 					BlockTimestamp:       blockTimestamp,
 					TokenContractName:    tokenContractName,
 					TokenContractSymbol:  tokenContractSymbol,
+					TransactionFee:       transactionFee,
+					NftId:                nftId,
+				}
+
+				tokenTransfers = append(tokenTransfers, tokenTransfer)
+			} else if logETL.Indexed[0] == "TransferSingle(Address,Address,Address,int,int)" {
+				// TransferSingle is not a protected method
+				// Handle IRC31 transfers single
+
+				// Token Contract Address
+				tokenContractAddress := logETL.Address
+
+				// From Address
+				fromAddress := logETL.Indexed[1]
+
+				// To Address
+				toAddress := logETL.Indexed[2]
+
+				// Value
+				value, valueDecimal := findHighestIcxTransfer(transactionETL.Logs)
+
+				// NFT ID
+				var nftId int64
+				if len(logETL.Data) == 2 {
+					nftId = utils.StringHexToInt64(logETL.Data[0])
+				} else {
+					continue
+				}
+
+				// Block Timestamp
+				blockTimestamp := blockETL.Timestamp
+
+				// Token Contract Name
+				tokenContractName, err := service.IconNodeServiceGetTokenContractName(tokenContractAddress)
+				if err != nil {
+					zap.S().Fatal(err)
+				}
+
+				// Transaction Fee
+				stepPriceBig := big.NewInt(0)
+				if transactionETL.StepPrice != "" {
+					stepPriceBig.SetString(transactionETL.StepPrice[2:], 16)
+				}
+				stepUsedBig := big.NewInt(0)
+				if transactionETL.StepUsed != "" {
+					stepUsedBig.SetString(transactionETL.StepUsed[2:], 16)
+				}
+				transactionFeeBig := stepUsedBig.Mul(stepUsedBig, stepPriceBig)
+				transactionFee := fmt.Sprintf("0x%x", transactionFeeBig)
+
+				tokenTransfer := &models.TokenTransfer{
+					TransactionHash:      transactionETL.Hash,
+					LogIndex:             int64(iL),
+					TokenContractAddress: tokenContractAddress,
+					FromAddress:          fromAddress,
+					ToAddress:            toAddress,
+					BlockNumber:          blockETL.Number,
+					Value:                value,
+					ValueDecimal:         valueDecimal,
+					BlockTimestamp:       blockTimestamp,
+					TokenContractName:    tokenContractName,
 					TransactionFee:       transactionFee,
 					NftId:                nftId,
 				}
