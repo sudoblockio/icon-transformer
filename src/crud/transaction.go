36907e768bb50c8900841a83e7d586017814ee58
robcxyz <rob@sudoblock.io>
2022-10-03 21:59:58 +0530

    chore: tune transactions crud

--- src/crud/transaction.go
+++ src/crud/transaction.go
@@ -2,6 +2,7 @@ package crud
 
 import (
 	"sync"
+	"time"
 
 	"github.com/sudoblockio/icon-transformer/models"
 )
@@ -17,6 +18,7 @@ func GetTransactionCrud() *Crud[models.Transaction, models.TransactionORM] {
 
 		TransactionCrud.Migrate()
 		TransactionCrud.CreateIndexes("CREATE INDEX IF NOT EXISTS transaction_idx_block_number_type_hash ON public.transactions USING btree (block_number, type, hash)")
+		TransactionCrud.dbBufferWait = 10 * time.Millisecond
 
 		TransactionCrud.columns = removeColumnNames(TransactionCrud.columns, []string{"transaction_type"})
 		TransactionCrud.MakeStartLoaderChannel()
@@ -24,7 +26,7 @@ func GetTransactionCrud() *Crud[models.Transaction, models.TransactionORM] {
 		// Used in transactionCreateScores transformer
 		TransactionTypeCrud = GetCrud(models.Transaction{}, models.TransactionORM{})
 		TransactionTypeCrud.columns = []string{"hash", "transaction_type", "score_address", "log_index"}
-		TransactionTypeCrud.metrics.Name = TransactionCrud.TableName + "types"
+		TransactionTypeCrud.metrics.Name = TransactionCrud.TableName + "_types"
 		TransactionTypeCrud.MakeStartLoaderChannel()
 	})
 
