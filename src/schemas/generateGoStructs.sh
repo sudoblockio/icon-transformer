59f43d85d9790662782731cff94826a9765c205a
robcxyz <rob@sudoblock.io>
2022-10-16 00:25:42 +0530

    chore: upddate gen script

--- src/schemas/generateGoStructs.sh
+++ src/schemas/generateGoStructs.sh
@@ -3,7 +3,7 @@ set -e
 # For compiling issues
 # https://www.notion.so/sudoblock/Protobuf-d5efc374891a452798e3f3a414722eec
 
-# This could be run to nuke it all
+# This could be run to nuke it all`
 #echo "Remove old models..."
 #rm ../models/*
 cd "$(dirname "$0")"
@@ -29,32 +29,20 @@ protoc \
   --go_out=.. \
   --gorm_out="engine=postgres:.." \
   !(*contract_processed).proto
-#  address.proto
-
-#  --go_out=.. \
-#  --proto_path=${GOPATH}/src/github.com/gogo/protobuf/protobuf \
-#  --gogofaster_out=. \
-#  --gogo_out=. \
-
 
 # For contract_processed.proto which has an optional field
-#protoc \
-#  --proto_path=. \
-#  --proto_path=${GOPATH}/src \
-#  --proto_path=${GOPATH}/src/github.com/mwitkow/go-proto-validators \
-#  --go_out=.. \
-#  contract_processed.proto
-#
-## Remove omitempty option
-## Credit: https://stackoverflow.com/a/37335452
-#ls ../models/*.pb.go | xargs -n1 -IX bash -c 'sed s/,omitempty// X > X.tmp && mv X{.tmp,}'
-#
-#echo "Completed proto to struct..."
+protoc \
+  --proto_path=. \
+  --proto_path=${GOPATH}/src \
+  --proto_path=${GOPATH}/src/github.com/mwitkow/go-proto-validators \
+  --go_out=.. \
+  contract_processed.proto
 
-for filename in ../models/*.pb.go
-do
-    sed -i '/^[ \t]XXX/d' ${filename}
-done
+# Remove omitempty option
+# Credit: https://stackoverflow.com/a/37335452
+ls ../models/*.pb.go | xargs -n1 -IX bash -c 'sed s/,omitempty// X > X.tmp && mv X{.tmp,}'
+# Remove unwanted `XXX_*` fields
+for filename in ../models/*.pb.go; do sed -i '/^[ \t]XXX/d' ${filename}; done
+# TODO: Remove `XXX_` methods
 
-
-#sed -i '/^[ \t]XXX/d' address.pb.go
+echo "Completed proto to struct..."
